05a44f55d925e2454cd0baf7ac674e8839ee4d43
mzvast <mzvast@gmail.com>
2019-07-29 02:09:53 +0800

    feat: add post start-loving-chnage

---
+++ src/routes/posts/2019/07/29-start-loving-change/post.ts
@@ -0,0 +1,6 @@
+export default {
+  title: `Start Loving Change`,
+  tags: ['life', 'strategy'],
+  spoiler: 'If Nothing Changes, Nothing Is Going to Change',
+  getContent: () => import('./document.mdx'),
+}
