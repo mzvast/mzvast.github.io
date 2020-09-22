53137d80e8611bf5a40eb60b45f70bc28af79f6e
mzvast <mzvast@gmail.com>
2020-09-22 22:37:46 +0800

    fix&chore getDate align width

--- .hygen.js
+++ .hygen.js
@@ -3,9 +3,11 @@ module.exports = {
         getDate: () => {
             let d = new Date();
             let year = d.getFullYear();
-            let month = ('0' + d.getMonth() + 1).slice(-2);
+            let month = ('0' + (d.getMonth() + 1)).slice(-2);
             let day = ('0' + d.getDate()).slice(-2);
-            return `${year}/${month}/${day}`;
+            let hour = ('00' + d.getHours()).slice(-2);
+            let min = ('00' + d.getMinutes()).slice(-2);
+            return `${year}/${month}/${day}-${hour}${min}`;
         }
     }
 };
