fcc740ab399474b6eb080e53a43232584bed9043
mzvast <mzvast@gmail.com>
2020-09-23 01:11:37 +0800

    feat add the most successful developers share more than they take

---
+++ src/routes/posts/2020/09/23-0034-the-most-successful-developers-share-more-than-they-take/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'the most successful developers share more than they take',
+    tags: ["life"],
+    spoiler: '',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
