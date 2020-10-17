f6e59fadc0244326392835a653490b8b6a4366c0
mzvast <mzvast@gmail.com>
2020-10-18 01:50:20 +0800

    add official-project-review

---
+++ src/routes/posts/2020/10/17-2313-official-project-review/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'official website project review',
+    tags: ["react", "webpack"],
+    spoiler: 'Review of our latest project',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
