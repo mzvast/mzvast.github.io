a1985fd2609fe162f3e8534b9c1016e6a50f78cb
mzvast <mzvast@gmail.com>
2020-11-07 23:06:22 +0800

    feat: add debug react ts

---
+++ src/routes/posts/2020/11/07-2114-how-to-debug-react-without-pain/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'how to debug react without pain',
+    tags: ["react","webpack"],
+    spoiler: 'debug react without pain',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
