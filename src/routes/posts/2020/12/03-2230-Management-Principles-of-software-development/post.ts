c9f4d69188d03d12352502e05990ab8ada5f81ed
mzvast <mzvast@gmail.com>
2020-12-03 23:33:50 +0800

    add post 软件开发的管理原则

---
+++ src/routes/posts/2020/12/03-2230-Management-Principles-of-software-development/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'Management Principles of software development',
+    tags: ["dev"],
+    spoiler: '软件开发的管理原则',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
