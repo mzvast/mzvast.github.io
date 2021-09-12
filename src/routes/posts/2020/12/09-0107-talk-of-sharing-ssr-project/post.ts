dcb42f1863cf02c79a541fd955d287f26cec7bd9
mzvast <mzvast@gmail.com>
2021-09-12 14:32:40 +0800

    add posts

---
+++ src/routes/posts/2020/12/09-0107-talk-of-sharing-ssr-project/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'talk of sharing ssr project',
+    tags: ["review","react","ssr"],
+    spoiler: 'rt',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
---
+++ src/routes/posts/2020/12/14-0059-what-we-can-learn-from-Dao-Sheng-He-Fu/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'what we can learn from Dao Sheng He Fu',
+    tags: ["life"],
+    spoiler: '读稻盛和夫',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
