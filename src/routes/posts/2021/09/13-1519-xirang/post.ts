cec366dccd5c0a695be52db95ed9f1ad464e7ad5
mzvast <mzvast@gmail.com>
2021-09-13 19:15:49 +0800

    add graph

---
+++ src/routes/posts/2021/09/13-1519-xirang/post.ts
@@ -0,0 +1,10 @@
+export default {
+    title: 'xirang',
+    tags: ["xirang"],
+    spoiler: '',
+    getContent: () => import('./document.mdx'),
+    // getContent: () => import('./项目管理.mdx'),
+}
+
+
+
