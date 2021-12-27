32cd0acbb80b54961eca022b7c7000b46fad048e
mzvast <mzvast@gmail.com>
2021-12-27 21:49:05 +0800

    feat: add fix git merge

---
+++ src/routes/posts/2021/12/27-2027-Fix-Git-Merge-Mis-Deleting-files/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'Fix Git Merge Mis-Deleting files',
+    tags: ["git"],
+    spoiler: 'How to recover from mis-deleting files from a git merge commit',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
