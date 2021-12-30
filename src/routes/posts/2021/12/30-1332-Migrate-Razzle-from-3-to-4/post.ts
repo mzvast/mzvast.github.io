49be83d2ed12c541ba7a04afb0319da292454d7c
mzvast <mzvast@gmail.com>
2021-12-30 14:18:03 +0800

    feat: add Migrate-Razzle-from-3-to-4

---
+++ src/routes/posts/2021/12/30-1332-Migrate-Razzle-from-3-to-4/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'Migrate Razzle from 3 to 4',
+    tags: ["razzle"],
+    spoiler: 'my experience of doing migration razzle to version 4 ',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
