d5ddef5d0f9ad850aa4f4274829bf1786131269b
mzvast <mzvast@gmail.com>
2022-01-12 20:59:57 +0800

    add iCopy

---
+++ src/routes/posts/2022/01/12-1915-iCopy-release-new-version/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'iCopy release new version',
+    tags: ["iCopy"],
+    spoiler: 'iCopy support new Clipboard API now, how we intercept API call',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
