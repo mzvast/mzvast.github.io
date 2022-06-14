43c4d77fd2b75dd1eefedb04612308ffb6e39f72
mzvast <mzvast@gmail.com>
2022-06-14 14:32:29 +0800

    add some

---
+++ src/routes/posts/2022/06/14-1331-how-to-build-a-level-logger/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'how to build a level logger',
+    tags: ["javascript","logger"],
+    spoiler: 'level logger',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
