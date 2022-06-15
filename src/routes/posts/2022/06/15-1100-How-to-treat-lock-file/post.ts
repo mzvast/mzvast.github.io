53b734769a589b44bd2a9500e2eabe050d64eeb8
mzvast <mzvast@gmail.com>
2022-06-15 11:27:02 +0800

    add How to treat lock file

---
+++ src/routes/posts/2022/06/15-1100-How-to-treat-lock-file/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'How to treat lock file',
+    tags: ["npm","yarn","lock"],
+    spoiler: 'how to make lock work again',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
