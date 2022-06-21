07038b84109b2fcfe9a016380192dc56e47c2a1d
mzvast <mzvast@gmail.com>
2022-06-21 18:27:04 +0800

    add OO plugin

---
+++ src/routes/posts/2022/06/21-1725-omni-outliner-plugin-try-try/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'omni outliner plugin try try',
+    tags: ["omni outliner","plugin"],
+    spoiler: 'how to make an omni outliner plugin',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
