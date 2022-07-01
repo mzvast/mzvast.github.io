df7e3e31fc21aeadb7e0551d242af758fe35aa0d
mzvast <mzvast@gmail.com>
2022-07-01 11:33:26 +0800

    add Switch借助xray透明代理达成NAT full cone

---
+++ src/routes/posts/2022/07/01-1024-Switch借助xray透明代理达成NAT-full-cone/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'Switch借助xray透明代理达成NAT full cone',
+    tags: ["switch","xray","NAT","p2p","network","UDP"],
+    spoiler: 'how to make NS better at p2p gaming',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
