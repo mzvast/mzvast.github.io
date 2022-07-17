b05ed228621fad682998343f66687ebc819af942
mzvast <mzvast@gmail.com>
2022-07-17 18:59:09 +0800

    add usd

---
+++ src/routes/posts/2022/07/17-1351-from-zero-to-USD/post.ts
@@ -0,0 +1,9 @@
+export default {
+    title: 'from zero to USD',
+    tags: ["usd","c++","python"],
+    spoiler: 'how to setup a mac for USD',
+    getContent: () => import('./document.mdx'),
+}
+
+
+
